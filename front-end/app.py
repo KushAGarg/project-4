e5fdd4e5066dc81cd25d1ba95f4d4fb565842d9b
rohanhpatel <rohan.h.patel@gmail.com>
2023-12-11 20:47:47 -0600

    Updated front end to update value using nn model and also store values in MongoDB to pull if repeat parameters are used

--- front-end/app.py
+++ front-end/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from flask_cors import CORS
+from bs4 import BeautifulSoup
+import tensorflow as tf
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -13,19 +16,84 @@ mongo = PyMongo(app)
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['Housing']
-collection = db['Houses']
+houses = db['Houses']
+
+if "FakeHouses" in db.list_collection_names():
+    db['FakeHouses'].drop()
+fake_houses = db['FakeHouses']
+
+model = tf.keras.models.load_model(Path("./neural-network/models/optimized_model_5.h5"))
+print(model.summary())
+
+def transform_input(sqft, bed, bath, loc, year):
+    ret = [sqft, bed, bath, year]
+    if loc.lower() == "rural":
+        ret += [1, 0, 0]
+    elif loc.lower() == "suburban":
+        ret += [0, 1, 0]
+    elif loc.lower() == "urban":
+        ret += [0, 0, 1]
+    else:
+        return []
+    return ret
+
+def assignH3String(s):
+    f = open(Path("./front-end/templates/index.html"), "r")
+    soup = BeautifulSoup(f.read())
+    f.close()
+
+    parent = soup.find("div", class_ = "left-part")
+    parent.h3.string = s
+
+    f = open(Path("./front-end/templates/index.html"), "w")
+    f.write(str(soup))
+    f.close()
+
+def assignH4String(s):
+    f = open(Path("./front-end/templates/index.html"), "r")
+    soup = BeautifulSoup(f.read())
+    f.close()
+
+    parent = soup.find("div", class_ = "left-part")
+    parent.h4.string = s
+
+    f = open(Path("./front-end/templates/index.html"), "w")
+    f.write(str(soup))
+    f.close()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    assignH3String("")
+    assignH4String("")
     if request.method == 'POST':
+        assignH3String("")
+        assignH4String("")
         data = {
-            'square_feet': request.form.get('square_feet'),
-            'bedrooms': request.form.get('bedrooms'),
-            'bathrooms': request.form.get('bathrooms'),
-            'neighborhood': request.form.get('neighborhood'),
-            'year_built': request.form.get('year_built'),
+            'SquareFeet': int(request.form.get('square_feet')),
+            'Bedrooms': int(request.form.get('bedrooms')),
+            'Bathrooms': int(request.form.get('bathrooms')),
+            'Neighborhood': request.form.get('neighborhood'),
+            'YearBuilt': int(request.form.get('year_built')),
         }
-        collection.insert_one(data)
+        
+        price_output = None
+        found_data = fake_houses.find(data)
+        elem = None
+        for x in found_data:
+            elem = x
+        print("NEXT")
+        if elem != None:
+            assignH4String("Price pulled from Mongo")
+            price_output = elem['Price']
+        else:
+            vals = list(data.values())
+            inp = transform_input(vals[0], vals[1], vals[2], vals[3], vals[4])
+            price_output = round(float(model.predict([inp])[0][0]), 2)
+        data['Price'] = price_output
+        
+        assignH3String(str(price_output))
+        
+        output = fake_houses.insert_one(data)
     return render_template('index.html')
 
 if __name__ == '__main__':
